523aed56ededed3513a9296b05acfb0548096fbc
Amelia Lim <amelialimyh@gmail.com>
2021-09-13 16:11:11 +0800

    completed MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :movie
-  root to: 'movies#home'
+  root to: 'lists#index'
 
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
