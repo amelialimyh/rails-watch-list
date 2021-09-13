f9106a0d69773a4c0dabf0a7d0af41fc4b2dbeae
Amelia Lim <amelialimyh@gmail.com>
2021-09-13 12:42:45 +0800

    edited database and gem file

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,23 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @new_movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+end
